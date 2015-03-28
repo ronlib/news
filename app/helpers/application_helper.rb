fd35caee47ea2f3a7442c508dcbf10a8527e7437
Ron <ronll64@gmail.com>
2015-03-28 11:54:12 +0300

    Tried to use configuration to set the title of the page. Still not working

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,11 @@ module ApplicationHelper
   
   # Returns the full title  
   def full_title(page_title = '') 
-    base_title
+    base_title = Rails.configuration.base_title
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
   end
 end
