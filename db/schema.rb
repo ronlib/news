496e70a47748c94d8849bd7b21a33966b2de4e34
Ron <ronll64@gmail.com>
2015-04-27 23:07:52 +0300

    Added users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150316145619) do
+ActiveRecord::Schema.define(version: 20150426173909) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "name"
@@ -41,5 +41,14 @@ ActiveRecord::Schema.define(version: 20150316145619) do
   end
 
   add_index "comments", ["article_id"], name: "index_comments_on_article_id"
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email"
 
 end
